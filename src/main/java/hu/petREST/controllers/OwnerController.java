472ad40bfbfba052fb196a4dc66655f2f3f97648
20g_dankoi <dankopisti16@gmail.com>
2021-12-10 09:22:03 +0100

    commit

--- src/main/java/hu/petREST/controllers/OwnerController.java
+++ src/main/java/hu/petREST/controllers/OwnerController.java
@@ -3,8 +3,10 @@ package hu.petREST.controllers;
 import hu.petREST.domain.Owner;
 import hu.petREST.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,7 @@ public class OwnerController {
     private OwnerService service;
 
     @PostMapping("/owners")
+    @ResponseStatus(HttpStatus.CREATED)
     public Owner addOwner(@RequestBody Owner owner){
         return service.addOwner(owner);
     }
